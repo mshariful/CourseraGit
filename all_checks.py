307df4a69d860cea1246152c0609f0332ec1d7b5
Dr. Shariful Islam <endromida@gmail.com>
2023-05-08 12:09:34 +0200

    Rename min_absolute to min_gb, use parameters names

--- all_checks.py
+++ all_checks.py
@@ -11,14 +11,14 @@ def check_reboot():
 
 # This is another comment line
 
-def check_dlsk_full(disk, min_absolute, min_percent):
+def check_dlsk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False othemise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of the free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return True
 
     return False
@@ -29,7 +29,13 @@ def main():
         print ("Pending Reboot.")
         sys.ex1t(1)
 
-    if check_disk_full("/", 2, 10):
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk full.")
+        sys.exit(1)
+
+    print("Everythin ok")
+    sys.exit(0)
+
+main()
 
 
